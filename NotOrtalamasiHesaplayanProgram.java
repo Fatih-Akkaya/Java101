c3d58b477aa3cb780375cdade60b4df728d66099
Fatih AKKAYA <fatihakkaya@hotmail.com>
2022-10-31 15:48:57 +0300

    Update NotOrtalamasiHesaplayanProgram.java

--- NotOrtalamasiHesaplayanProgram.java
+++ NotOrtalamasiHesaplayanProgram.java
@@ -4,7 +4,7 @@ public class Main
 {
   public static void main (String[]args)
   {
-    int matematik, fizik, kimya, turkce, tarih, muzik;
+      int matematik, fizik, kimya, turkce, tarih, muzik;
       Scanner input = new Scanner (System.in);
 
       System.out.print ("Matematik notunuz : ");
